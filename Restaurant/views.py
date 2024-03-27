b846261d240d8104362e72e03e605b40fad06633
luisfer123 <luisfermtnz@gmail.com>
2024-03-26 20:50:18 -0600

    Adding token authentication and authorization

--- Restaurant/views.py
+++ Restaurant/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
 from . import models
 from . import serializers
 
@@ -12,13 +13,16 @@ def index(request):
     return render(request, 'index.html', {})
 
 class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = models.Menu.objects.all()
     serializer_class = serializers.MenuSerializer
     
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = models.Menu.objects.all()
     serializer_class = serializers.MenuSerializer
     
 class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = models.Booking.objects.all()
     serializer_class = serializers.BookingSerializer
